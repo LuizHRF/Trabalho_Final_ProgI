1da10200b43406c68a591f4aff799f85e832701b
Robert Drey <dreyrobert@gmail.com>
2023-06-19 15:07:07 -0300

    Add files via upload

--- Comanda/src/views/novoPedido.java
+++ Comanda/src/views/novoPedido.java
@@ -1,6 +1,8 @@
 package views;
 
+import entities.Pedido;
 import javax.swing.DefaultComboBoxModel;
+import models.ModelPedido;
 import models.ModelProduto;
 
 /*
@@ -52,32 +54,22 @@ public class novoPedido extends javax.swing.JFrame {
         cbEntrada = new javax.swing.JComboBox<>();
         cbBebida = new javax.swing.JComboBox<>();
         cbSobremesa = new javax.swing.JComboBox<>();
-        btnVoltar1 = new javax.swing.JButton();
+        btnVoltar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        spMesa.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-
-        jLabel1.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
         jLabel1.setText("Mesa");
 
-        edtAtendente.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        edtAtendente.setForeground(new java.awt.Color(51, 51, 51));
         edtAtendente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 edtAtendenteActionPerformed(evt);
             }
         });
 
-        jLabel2.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
         jLabel2.setText("Atendente");
 
-        btnSalvar.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        btnSalvar.setForeground(new java.awt.Color(51, 51, 51));
         btnSalvar.setText("Salvar");
         btnSalvar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -85,24 +77,14 @@ public class novoPedido extends javax.swing.JFrame {
             }
         });
 
-        jLabel4.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(51, 51, 51));
         jLabel4.setText("Sobremesa");
 
-        jLabel7.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(51, 51, 51));
         jLabel7.setText("Bebida");
 
-        jLabel8.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        jLabel8.setForeground(new java.awt.Color(51, 51, 51));
         jLabel8.setText("Principal");
 
-        jLabel3.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(51, 51, 51));
         jLabel3.setText("Entrada");
 
-        cbPrincipal.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        cbPrincipal.setForeground(new java.awt.Color(51, 51, 51));
         cbPrincipal.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbPrincipal.setSelectedItem(modelPrincipal);
         cbPrincipal.addActionListener(new java.awt.event.ActionListener() {
@@ -111,8 +93,6 @@ public class novoPedido extends javax.swing.JFrame {
             }
         });
 
-        cbEntrada.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        cbEntrada.setForeground(new java.awt.Color(51, 51, 51));
         cbEntrada.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbEntrada.setSelectedItem(modelEntrada);
         cbEntrada.addActionListener(new java.awt.event.ActionListener() {
@@ -121,8 +101,6 @@ public class novoPedido extends javax.swing.JFrame {
             }
         });
 
-        cbBebida.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        cbBebida.setForeground(new java.awt.Color(51, 51, 51));
         cbBebida.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbBebida.setSelectedItem(modelBebida);
         cbBebida.addActionListener(new java.awt.event.ActionListener() {
@@ -131,8 +109,6 @@ public class novoPedido extends javax.swing.JFrame {
             }
         });
 
-        cbSobremesa.setFont(new java.awt.Font("Noto Sans CJK HK", 0, 18)); // NOI18N
-        cbSobremesa.setForeground(new java.awt.Color(51, 51, 51));
         cbSobremesa.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbSobremesa.setSelectedItem(modelSobremesa);
         cbSobremesa.addActionListener(new java.awt.event.ActionListener() {
@@ -141,11 +117,10 @@ public class novoPedido extends javax.swing.JFrame {
             }
         });
 
-        btnVoltar1.setFont(new java.awt.Font("Jamrul", 0, 18)); // NOI18N
-        btnVoltar1.setText("<");
-        btnVoltar1.addActionListener(new java.awt.event.ActionListener() {
+        btnVoltar.setText("<");
+        btnVoltar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnVoltar1ActionPerformed(evt);
+                btnVoltarActionPerformed(evt);
             }
         });
 
@@ -175,19 +150,19 @@ public class novoPedido extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(edtAtendente, javax.swing.GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE))
+                                .addComponent(edtAtendente, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE))
                             .addComponent(cbPrincipal, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(cbSobremesa, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(cbBebida, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(btnVoltar1)
-                .addGap(0, 0, Short.MAX_VALUE))
+                            .addComponent(cbBebida, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(btnVoltar, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(btnVoltar1)
+                .addComponent(btnVoltar, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
@@ -195,22 +170,22 @@ public class novoPedido extends javax.swing.JFrame {
                     .addComponent(edtAtendente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cbEntrada, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3)
+                    .addComponent(cbEntrada, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel8)
                     .addComponent(cbPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(cbSobremesa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cbBebida, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel7))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel7)
+                    .addComponent(cbBebida, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, 18, Short.MAX_VALUE)
                 .addComponent(btnSalvar))
         );
 
@@ -220,14 +195,15 @@ public class novoPedido extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 8, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -238,7 +214,21 @@ public class novoPedido extends javax.swing.JFrame {
     }//GEN-LAST:event_edtAtendenteActionPerformed
 
     private void btnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarActionPerformed
-
+        double valor = ModelProduto.getValor((String)cbEntrada.getSelectedItem()) + ModelProduto.getValor((String)cbPrincipal.getSelectedItem()) + ModelProduto.getValor((String)cbSobremesa.getSelectedItem()) + ModelProduto.getValor((String)cbBebida.getSelectedItem()) ;
+        
+        Pedido p = new Pedido(
+                edtAtendente.getText(),
+                Integer.parseInt(spMesa.getValue().toString()),
+                (String)cbEntrada.getSelectedItem(),
+                (String)cbPrincipal.getSelectedItem(),
+                (String)cbSobremesa.getSelectedItem(),
+                (String)cbBebida.getSelectedItem(),
+                false,
+                valor
+        );
+        ModelPedido.getPedidos().add(p);
+        this.btnVoltarActionPerformed(evt);
+        
     }//GEN-LAST:event_btnSalvarActionPerformed
 
     private void cbPrincipalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbPrincipalActionPerformed
@@ -257,11 +247,11 @@ public class novoPedido extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_cbSobremesaActionPerformed
 
-    private void btnVoltar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltar1ActionPerformed
+    private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
         Comandas p = new Comandas();
         this.dispose();
         p.setVisible(true);
-    }//GEN-LAST:event_btnVoltar1ActionPerformed
+    }//GEN-LAST:event_btnVoltarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -300,7 +290,7 @@ public class novoPedido extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSalvar;
-    private javax.swing.JButton btnVoltar1;
+    private javax.swing.JButton btnVoltar;
     private javax.swing.JComboBox<String> cbBebida;
     private javax.swing.JComboBox<String> cbEntrada;
     private javax.swing.JComboBox<String> cbPrincipal;
